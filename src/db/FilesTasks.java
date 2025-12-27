5d70c98f7e97395f69ce09f6c7ebfac74c753cbb
salcines <salcines@outlook.es>
2025-12-27 03:21:55 +0100

    XMAS-5 Day Two. On the second day of Coding... Locate the average sweetness from two cocoa closest to a target sweetness

--- src/db/FilesTasks.java
+++ src/db/FilesTasks.java
@@ -4,4 +4,7 @@ public class FilesTasks {
     public String getTask1Dataset() {
         return "Resources/dataset-task-1.txt";
     }
+    public String getTask2Dataset() {
+        return "Resources/dataset-task-2.txt";
+    }
 }
